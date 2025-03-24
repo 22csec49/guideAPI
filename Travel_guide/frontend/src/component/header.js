d36177c22384d0080508b1f21b9b23a3732d52fc
22csec49 <22csec49@gmail.com>
2025-03-24 14:18:34 +0530

    header added

--- Travel_guide/frontend/src/component/header.js
+++ Travel_guide/frontend/src/component/header.js
@@ -2,6 +2,21 @@ import React from "react";
 import "./../styles/Header.css";
 
 const Header = () => {
+  const handleSelection = (event) => {
+    const urlMap = {
+      flight: "https://www.airindia.com/en-in/book-flights/",
+      train: "https://www.irctc.co.in/",
+      bus: "https://www.tnstc.in/home.html",
+      hotel: "https://www.makemytrip.com/hotels/",
+      car: "https://www.zoomcar.com/",
+    };
+
+    const selectedOption = event.target.value;
+    if (selectedOption && urlMap[selectedOption]) {
+      window.location.href = urlMap[selectedOption]; // Redirects to the selected booking site
+    }
+  };
+
   return (
     <header className="header">
       <div className="logo">🌍 TravelGo</div>
@@ -9,8 +24,19 @@ const Header = () => {
         <ul>
           <li><a href="#">Home</a></li>
           <li><a href="#">Destinations</a></li>
-          <li><a href="#">Bookings</a></li>
           <li><a href="#">Contact</a></li>
+          <li>
+            <a>
+            <select id="booking" name="booking" onChange={handleSelection}>
+              <option value="">Travel partners</option>
+              <option value="flight">AIR INDIA</option>
+              <option value="train">IRCTC</option>
+              <option value="bus">TNSTC</option>
+              <option value="hotel">MAKE MY TRIP</option>
+              <option value="car">ZOOM CARS</option>
+            </select>
+            </a>
+          </li>
         </ul>
       </nav>
       <button className="login-btn">Sign In</button>
